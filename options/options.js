6207b8b6d7daa22a07dd23ca7fc8931b185934bf
John Bender <john.b@crowdbotics.com>
2023-04-14 09:31:20 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "ai-codegen-test-app-39930",
-  url: "https://ai-codegen-test-app-39930.botics.co",
-  api: "https://ai-codegen-test-app-39930.botics.co/api/v1"
-}
+  "name": "ai-codegen-test-app-39930",
+  "url": "https://ai-codegen-test-app-39930.botics.co",
+  "api": "https://ai-codegen-test-app-39930.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
